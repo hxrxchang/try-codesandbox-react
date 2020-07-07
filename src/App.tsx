78e3d4b328edb4446b1f70e8747aaef69e21cbe8
Yuto Hara <m.y.y.0326.cg@gmail.com>
2020-07-08 02:09:34 +0900

    use hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import * as React from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = React.useState<number>(0);
   return (
     <div className="App">
+      {count}
+      <button onClick={() => setCount(count + 1)}>click</button>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
